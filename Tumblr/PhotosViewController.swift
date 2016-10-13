8bb53d0344494202038cabaa232ddaa3370450c5
Carina Boo <cboo@cboo-macbookpro.roam.corp.google.com>
2016-10-12 21:04:00 -0700

    Sticky headers implemented.

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -12,7 +12,7 @@ import AFNetworking
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
   var posts: [NSDictionary]?
-  var refreshControl: UIRefreshControl!
+//  var refreshControl: UIRefreshControl!
 
   @IBOutlet weak var postTableView: UITableView!
   @IBOutlet weak var postTableViewCell: PostTableViewCell!
@@ -26,19 +26,27 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     
     self.postTableView.rowHeight = 320
     
-    self.refreshControl = UIRefreshControl()
-    self.refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
     
-    getPosts()
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+    
+    getPosts {
+      self.postTableView.reloadData()
+    }
     
     postTableView.insertSubview(refreshControl, at: 0)
   }
   
-  func refreshControlAction(_:Any?) {
-    getPosts()
+  func refreshControlAction(refreshControl:UIRefreshControl) {
+    
+    getPosts {
+      self.postTableView.reloadData()
+      refreshControl.endRefreshing()
+    }
+    
   }
   
-  func getPosts() {
+  func getPosts(handler: @escaping () -> ()) {
     let apiKey = "Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"
     let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=\(apiKey)")
     let request = URLRequest(url: url!)
@@ -55,20 +63,23 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
           let response = responseDictionary["response"] as! NSDictionary
           self.posts = response["posts"] as? [NSDictionary]
           
-          self.postTableView.reloadData()
-          self.refreshControl.endRefreshing()
+          handler()
         }
       }
     });
     task.resume()
   }
   
-  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func numberOfSections(in tableView: UITableView) -> Int {
     if let posts = self.posts {
       return posts.count
     } else {
       return 0
     }
+  }
+  
+  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
   }
   
   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -84,6 +95,27 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+    headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+    let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+    profileView.clipsToBounds = true
+    profileView.layer.cornerRadius = 15;
+    profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
+    profileView.layer.borderWidth = 1;
+    // set the avatar
+    profileView.setImageWith(URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/avatar")!)
+    headerView.addSubview(profileView)
+    // Add a UILabel for the date here
+    // Use the section number to get the right URL
+//    let label = ...
+    return headerView
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 50
+  }
+  
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     let vc = segue.destination as! PhotosDetailsViewController
     let indexPath = self.postTableView.indexPath(for: sender as! UITableViewCell)
@@ -92,7 +124,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
   }
   
   func getPhotoURLFromPosts(indexPath: IndexPath) -> URL {
-    let post = self.posts![indexPath.row]
+    let post = self.posts![indexPath.section]
     
     let photos = post["photos"] as! [NSDictionary]
     let photo = photos[0]
